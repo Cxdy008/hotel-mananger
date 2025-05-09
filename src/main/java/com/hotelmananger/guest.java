cad58e1d3fd0c632777664322540bf3d6aab83c5
dizonovundu <dizonovundu@gmail.com>
2025-05-09 22:47:24 +0100

    Segundo commit - V1.0.1

---
+++ src/main/java/com/hotelmananger/guest.java
@@ -0,0 +1,11 @@
+package com.hotelmananger;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class guest {
+    String name;
+    String email;
+    String phone;
+    String address;
+}
